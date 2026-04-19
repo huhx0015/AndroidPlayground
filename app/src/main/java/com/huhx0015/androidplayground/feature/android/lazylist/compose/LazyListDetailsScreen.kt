50b997a776b2ee2e7006d309c56454217522bd5a
huhx0015 <mhuh@yoonhuh.com>
2026-04-19 13:31:09 -0400

    Finish implementation of passing Navigation argument to LazyListDetailsScreen
    Finish implementation of passing Navigation argument to LazyListDetailsScreen

--- app/src/main/java/com/huhx0015/androidplayground/feature/android/lazylist/compose/LazyListDetailsScreen.kt
+++ app/src/main/java/com/huhx0015/androidplayground/feature/android/lazylist/compose/LazyListDetailsScreen.kt
@@ -1,19 +1,31 @@
 package com.huhx0015.androidplayground.feature.android.lazylist.compose
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.huhx0015.androidplayground.model.DataItem
 
 @Composable
 internal fun LazyListDetailsScreen(
-    dataItem: DataItem,
+    dataItem: DataItem?,
     modifier: Modifier = Modifier
 ) {
-    Column(modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
         Text(
-            text = dataItem.title
+            text = dataItem?.title ?: "Title not found"
+        )
+        Text(
+            text = dataItem?.subtitle ?: "Subtitle not found"
         )
     }
+}
+
+@Preview
+@Composable
+private fun LazyListDetailsScreenPreview() {
+    LazyListDetailsScreen(dataItem = DataItem())
 }
--- app/src/main/java/com/huhx0015/androidplayground/feature/android/lazylist/compose/LazyListNavigationScreen.kt
+++ app/src/main/java/com/huhx0015/androidplayground/feature/android/lazylist/compose/LazyListNavigationScreen.kt
@@ -8,17 +8,18 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.huhx0015.androidplayground.feature.android.lazylist.LazyListViewModel
 import com.huhx0015.androidplayground.feature.android.lazylist.navigation.Screen
-import com.huhx0015.androidplayground.model.DataItem
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -26,7 +27,7 @@ fun LazyListNavigationScreen(
     modifier: Modifier = Modifier
 ) {
     val viewModel: LazyListViewModel = viewModel()
-    val state = viewModel.state.collectAsState()
+    val state = viewModel.state.collectAsStateWithLifecycle()
     val navController = rememberNavController()
 
     Column(modifier = modifier.fillMaxSize()) {
@@ -52,13 +53,23 @@ fun LazyListNavigationScreen(
             composable(route = Screen.List.route) {
                 LazyListScreen(
                     dataList = state.value.dataList,
-                    onRowClick = {
-                        navController.navigate(Screen.Detail.route)
+                    onRowClick = { dataItemId ->
+                        navController.navigate(Screen.Detail.createRoute(dataItemId))
                     }
                 )
             }
-            composable(route = Screen.Detail.route) {
-                LazyListDetailsScreen(dataItem = DataItem())
+            composable(
+                route = Screen.Detail.route,
+                arguments = listOf(
+                    navArgument(Screen.ARG_DATA_ITEM_ID) {
+                        type = NavType.IntType
+                    }
+                )
+            ) { backstackEntry ->
+                val dataItemId = backstackEntry.arguments?.getInt(Screen.ARG_DATA_ITEM_ID)
+                val dataItem = dataItemId?.let(viewModel::getDataItemById)
+
+                LazyListDetailsScreen(dataItem = dataItem)
             }
         }
     }
